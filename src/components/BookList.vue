d3b3ca5b5726bc107680a98c5c7ee6de908d7b80
KanamoriKouhei <glglntn6@gmail.com>
2021-04-25 21:20:15 +0900

    コンポーネント追加

---
+++ src/components/BookList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="booklist">
+    <h1>BookList</h1>
+    <ul>
+      <li v-for="book in books" :key="book.id">
+        <!-- リンク先の設定。 '<a href="/book/1/">book1</a>' -->
+        <router-link :to="{ name: 'book', params: { id: book.id }}">{{ book.name }}</router-link>
+      </li>
+    </ul>
+
+    <!-- /book/:id/* のURLに一致したコンポーネントが描画される -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BookList',
+  data: function() {
+    return {
+      books: [
+        {id: 1, name: 'book1'},
+        {id: 2, name: 'book2'},
+      ]
+    }
+  },
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,48 @@
 import Vue from 'vue'
+// Vue Routerのインポート
+import VueRouter from 'vue-router'
 import App from './App.vue'
+// コンポーネントのインポート
+import Index from './components/Index.vue'
+import BookList from './components/BookList.vue'
 
 Vue.config.productionTip = false
 
+// プラグインとして登録
+Vue.use(VueRouter)
+
+// ルートの定義
+const router = new VueRouter({
+  routes: [
+    // http://xxxxxxxx/ のURLのときはIndexのコンポーネントを使う
+    { path: '/', component: Index },
+    { path: '/book-list', component: BookList },
+
+    // http://xxxxxxxx/book/123 のURLのときはBookのコンポーネントを使う
+    // {
+    //   path: '/book/:id',
+    //   name: 'book',
+    //   component: Book,
+    //   // :idにあたる123は数値型にキャストしてコンポーネントに渡す
+    //   props: route => ({
+    //     id: Number(route.params.id)
+    //   }),
+    //   // http://xxxxxxxx/book/123/* のURLの定義
+    //   children: [
+    //     {
+    //       path: 'review/:review_id',
+    //       name: 'book-review',
+    //       component: BookReview,
+    //       props: route => ({
+    //         review_id: Number(route.params.review_id)
+    //       })
+    //     }
+    //   ]
+    // },
+  ]
+})
+
 new Vue({
+  router,  // ルーターの登録
   render: h => h(App),
 }).$mount('#app')
